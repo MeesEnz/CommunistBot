40be574b487a8736ccddb7b4941e2b46bbfcf9f4
MeesEnz <36102280+MeesEnz@users.noreply.github.com>
2018-12-24 18:08:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,28 @@ client.on('message', message => {
 
     if (message.author.bot) return;
 
+    exports.run = (client, message, args, tools) => {
 
+      // check for input
+      if (!args[0]) return message.channel.send('Please input a calculation.');
+    
+      // evalutate calculation
+      let resp;
+      try{
+        resp = math.eval(args.join(' '));
+      } catch (e) {
+        return message.channel.send('Sorry, please input a valid calculation.');
+      }
+    
+      // Send output
+      const embed = new Discord.MessageEmbed()
+        .setColor(0xffffff)
+        .setTitle('Math Calculation')
+        .addField('Input', `\`\`\`js\n${args.join('')}\`\`\``)
+        .addField('Output', `\`\`\`js\n${resp}\`\`\``)
+    
+      message.channel.send(embed);
+    }
 
     // !ping
     if (message.content.startsWith(prefix + "ping")) {
